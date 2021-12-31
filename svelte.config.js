c7ad1c6788117685497eee52a15af04acc624cf8
bato3 (work) <bato3@bandyci.org>
2021-12-31 08:44:34 +0100

    Add Kit adapter(s)

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 import autoprefixer from 'autoprefixer';
 import path from 'path';
@@ -6,44 +7,54 @@ import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: [
-		preprocess({
-			scss: {
-				prependData: '@use "src/variables.scss" as *;'
-			}
-		})
-	],
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: [
+        preprocess({
+            scss: {
+                prependData: '@use "src/variables.scss" as *;'
+            }
+        })
+    ],
 
-	kit: {
-		adapter: adapter(),
+    kit: {
+        adapter: adapter({
+            /*
+            // default options are shown
+            out: 'build',
+            precompress: false,
+            env: {
+                host: 'HOST',
+                port: 'PORT'
+            }
+            */
+        }),
 
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
+        // hydrate the <div id="svelte"> element in src/app.html
+        target: '#svelte',
 
-		vite: {
-			resolve: {
+        vite: {
+            resolve: {
                 alias: {
                     //'@lib': path.resolve('./src/lib'),
-					//'@': path.resolve('./node_modules'),
-					'$bs': path.resolve('./node_modules/bootstrap/scss'),
+                    //'@': path.resolve('./node_modules'),
+                    '$bs': path.resolve('./node_modules/bootstrap/scss'),
                 }
             },
-			css: {
-				preprocessorOptions: {
-					scss: {
-						additionalData: '@use "src/variables.scss" as *;'
-					}
-				},
-				postcss: {
-					plugins: [
-						autoprefixer()
-					]
-				}
-			}
-		}
-	}
+            css: {
+                preprocessorOptions: {
+                    scss: {
+                        additionalData: '@use "src/variables.scss" as *;'
+                    }
+                },
+                postcss: {
+                    plugins: [
+                        autoprefixer()
+                    ]
+                }
+            }
+        }
+    }
 };
 
 export default config;
